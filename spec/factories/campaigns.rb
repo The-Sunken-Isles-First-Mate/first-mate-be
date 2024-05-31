e50778d4e762ce91354186323c52561ffb639fa2
Billy Wallace <wallace.billy1@gmail.com>
2024-05-31 12:59:46 -0600

    Create Campaigns Factorybot

---
+++ spec/factories/campaigns.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :campaign do 
+    name { Faker::Lorem.words(2) }
+    week { Faker::Number.between(from: 0, to: 10) }
+    animal_products { Faker::Number.between(from: 0, to: 10) }
+    cloth { Faker::Number.between(from: 0, to: 10) }
+    farmed_goods { Faker::Number.between(from: 0, to: 10) }
+    food { Faker::Number.between(from: 0, to: 10) }
+    foraged_goods { Faker::Number.between(from: 0, to: 10) }
+    metal { Faker::Number.between(from: 0, to: 10) }
+    monster_parts { Faker::Number.between(from: 0, to: 10) }
+    stone { Faker::Number.between(from: 0, to: 10) }
+    wood { Faker::Number.between(from: 0, to: 10) }
+    villagers { Faker::Number.between(from: 100, to: 200) }
+  end
+end
