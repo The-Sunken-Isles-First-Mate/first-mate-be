1d18d01d3e038288aab7e84a9b54530a8ae5ad74
Billy Wallace <wallace.billy1@gmail.com>
2024-06-01 09:26:29 -0600

    Update request specs for new validations

--- spec/requests/api/v1/campaigns_request_spec.rb
+++ spec/requests/api/v1/campaigns_request_spec.rb
@@ -117,9 +117,22 @@ RSpec.describe "Campaigns API" do
   describe "Campaign Update" do 
     it "updates a campaigns attributes and returns the campaign" do
       id = create(:campaign).id
-      starting_wood = Campaign.last.wood
-      starting_villagers = Campaign.last.villagers
-      campaign_params = { wood: 10, villagers: 200 }
+      original_campaign = Campaign.last
+
+      # the below would represent updating the wood and villagers field while leaving the rest as is
+      campaign_params = { name: original_campaign.name,
+                          week: 0,
+                          animal_products: 0,
+                          cloth: 0,
+                          farmed_goods: 0,
+                          food: 0,
+                          foraged_goods: 0,
+                          metal: 0,
+                          monster_parts: 0,
+                          stone: 0,
+                          wood: 10,
+                          villagers: 200 }
+
       headers = {"CONTENT_TYPE" => "application/json"}
       
       patch "/api/v1/campaigns/#{id}", headers: headers, params: JSON.generate({campaign: campaign_params})
@@ -127,12 +140,52 @@ RSpec.describe "Campaigns API" do
       campaign = JSON.parse(response.body, symbolize_names: true)[:data]
 
       expect(response).to be_successful
+      expect(campaign[:attributes][:name]).to eq(original_campaign.name)
+      expect(campaign[:attributes][:stone]).to eq(original_campaign.stone)
       expect(campaign[:attributes][:wood]).to eq(10)
       expect(campaign[:attributes][:villagers]).to eq(200)
     end
 
-    it 'returns a 404 status and error message when an invalid vendor_id is passed in' do
-      campaign_params = { wood: 10 }
+    it 'returns a 422 status and error message when trying to update a field to nil' do
+      id = create(:campaign).id
+
+      campaign_params = { name: Campaign.last.name,
+                          week: 0,
+                          animal_products: "",
+                          cloth: 0,
+                          farmed_goods: 0,
+                          food: 0,
+                          foraged_goods: 0,
+                          metal: 0,
+                          monster_parts: 0,
+                          stone: 0,
+                          wood: 10,
+                          villagers: 200 }
+      headers = {"CONTENT_TYPE" => "application/json"}
+      
+      patch "/api/v1/campaigns/#{id}", headers: headers, params: JSON.generate({campaign: campaign_params})
+      
+      expect(response).to_not be_successful
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors].first[:status]).to eq("422")
+      expect(data[:errors].first[:title]).to eq("Validation failed: Animal products can't be blank, Animal products is not a number")
+    end
+
+    it 'returns a 404 status and error message when an invalid campaign_id is passed in' do
+      campaign_params = { name: Campaign.last.name,
+                          week: 0,
+                          animal_products: 0,
+                          cloth: 0,
+                          farmed_goods: 0,
+                          food: 0,
+                          foraged_goods: 0,
+                          metal: 0,
+                          monster_parts: 0,
+                          stone: 0,
+                          wood: 10,
+                          villagers: 200 }
       headers = {"CONTENT_TYPE" => "application/json"}
       
       patch "/api/v1/campaigns/123123", headers: headers, params: JSON.generate({campaign: campaign_params})
@@ -143,22 +196,51 @@ RSpec.describe "Campaigns API" do
 
       expect(data[:errors].first[:status]).to eq("404")
       expect(data[:errors].first[:title]).to eq("Couldn't find Campaign with 'id'=123123")
-
     end
 
-    it "returns a 400 status and error message when an trying to update to nil" do 
+    it "returns a 422 status and error message when an trying to update any of the integer fields with a non-integer value" do 
       id = create(:campaign).id
-      campaign_params = { wood: ""}
+      campaign_params = { wood: "one"}
       headers = {"CONTENT_TYPE" => "application/json"}
 
       patch "/api/v1/campaigns/#{id}", headers: headers, params:  JSON.generate({campaign: campaign_params})
 
       expect(response).to_not be_successful
-      expect(response.status).to eq(400)
+      expect(response.status).to eq(422)
       
       data = JSON.parse(response.body, symbolize_names: true)
 
-      expect(data[:errors].first[:title]).to eq("Validation failed: Wood can't be blank")
+      expect(data[:errors].first[:title]).to eq("Validation failed: Wood is not a number")
+    end
+
+    it "returns a 422 status and error message when an trying to update any of the integer fields with a number less than 0" do 
+      id = create(:campaign).id
+      campaign_params = { wood: -1}
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/campaigns/#{id}", headers: headers, params:  JSON.generate({campaign: campaign_params})
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+      
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors].first[:title]).to eq("Validation failed: Wood must be greater than or equal to 0")
+    end
+
+    it "returns a 422 status and error message when an trying to update any of the integer fields with a float" do 
+      id = create(:campaign).id
+      campaign_params = { wood: 1.1 }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/campaigns/#{id}", headers: headers, params:  JSON.generate({campaign: campaign_params})
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+      
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors].first[:title]).to eq("Validation failed: Wood must be an integer")
     end
   end
 end
