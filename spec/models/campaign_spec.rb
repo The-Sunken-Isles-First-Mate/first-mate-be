f5c6fcb52f0638e8fae6c6af87684698fed7bc9f
Billy Wallace <wallace.billy1@gmail.com>
2024-06-01 09:25:20 -0600

    Create validation tests for campaign

---
+++ spec/models/campaign_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Campaign, type: :model do
+  describe 'relationships' do
+
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:week)}
+    it {should validate_presence_of(:animal_products)}
+    it {should validate_presence_of(:cloth)}
+    it {should validate_presence_of(:farmed_goods)}
+    it {should validate_presence_of(:food)}
+    it {should validate_presence_of(:foraged_goods)}
+    it {should validate_presence_of(:metal)}
+    it {should validate_presence_of(:monster_parts)}
+    it {should validate_presence_of(:stone)}
+    it {should validate_presence_of(:wood)}
+    it {should validate_presence_of(:villagers)}
+
+    it {should validate_numericality_of(:week)}
+    it {should validate_numericality_of(:animal_products)}
+    it {should validate_numericality_of(:cloth)}
+    it {should validate_numericality_of(:farmed_goods)}
+    it {should validate_numericality_of(:food)}
+    it {should validate_numericality_of(:foraged_goods)}
+    it {should validate_numericality_of(:metal)}
+    it {should validate_numericality_of(:monster_parts)}
+    it {should validate_numericality_of(:stone)}
+    it {should validate_numericality_of(:wood)}
+    it {should validate_numericality_of(:villagers)}
+
+  end
+end
