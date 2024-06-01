ec43d592ebb9e3c5197315153986c1c08f862168
Billy Wallace <wallace.billy1@gmail.com>
2024-06-01 09:56:44 -0600

    Set up items_controller index action and route

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::ItemsController < ApplicationController
     item = Item.find(params[:id])
     render json: ItemSerializer.new(item)
   end
+
+  def index
+    campaign = Campaign.find(params[:campaign_id])
+    items = campaign.items
+    render json: ItemSerializer.new(items)
+  end
 end
