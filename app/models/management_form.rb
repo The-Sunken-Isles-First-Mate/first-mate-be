ee8df151b625341805b09a756188027500aa509e
Jared Hobson <jaredmhobson@gmail.com>
2024-06-01 11:51:35 -0700

    Add validations for ManagementForm model and model tests

--- app/models/management_form.rb
+++ app/models/management_form.rb
@@ -1,13 +1,22 @@
 class ManagementForm < ApplicationRecord
   belongs_to :campaign
 
-  validate :week_must_be_equal_or_less_than_campaign_week
+  validates :campaign_id, :week, :animal_products, :cloth, :farmed_goods,
+  :food, :foraged_goods, :metal, :monster_parts, :stone,
+  :wood, :light_armor, :medium_armor, :heavy_armor, :simple_weapon,
+  :martial_weapon, :ammunition, :adventuring_supplies, :assassins_blood,
+  :malice, :midnight_tears, :serpent_venom, :truth_serum, :oil_of_slipperiness,
+  :potion_of_climbing, :potion_of_healing, :potion_of_water_breathing,
+  :barge, :coracle, :double_hulled_sailing_canoe, :keelboat, :raft,
+  :single_hulled_sailing_canoe, :ballista, :cabin, :magical_defenses, :storage, presence: true
 
-  private
-
-  def week_must_be_equal_or_less_than_campaign_week
-    if week < campaign.week
-      errors.add(:week, "must be equal to or greater than the campaign week")
-    end
-  end
+validates :campaign_id, :week, :animal_products, :cloth, :farmed_goods, :food,
+  :foraged_goods, :metal, :monster_parts, :stone, :wood,
+  :light_armor, :medium_armor, :heavy_armor, :simple_weapon,
+  :martial_weapon, :ammunition, :adventuring_supplies, :assassins_blood,
+  :malice, :midnight_tears, :serpent_venom, :truth_serum, :oil_of_slipperiness,
+  :potion_of_climbing, :potion_of_healing, :potion_of_water_breathing,
+  :barge, :coracle, :double_hulled_sailing_canoe, :keelboat, :raft,
+  :single_hulled_sailing_canoe, :ballista, :cabin, :magical_defenses, :storage,
+  numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- spec/models/management_form_spec.rb
+++ spec/models/management_form_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe ManagementForm, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:campaign) }
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of(:campaign_id)}
+    it {should validate_presence_of(:week)}
+    it {should validate_presence_of(:animal_products)}
+    it {should validate_presence_of(:cloth)}
+    it {should validate_presence_of(:farmed_goods)}
+    it {should validate_presence_of(:food)}
+    it {should validate_presence_of(:foraged_goods)}
+    it {should validate_presence_of(:metal)}
+    it {should validate_presence_of(:monster_parts)}
+    it {should validate_presence_of(:stone)}
+    it {should validate_presence_of(:wood)}
+    it {should validate_presence_of(:light_armor)}
+    it {should validate_presence_of(:medium_armor)}
+    it {should validate_presence_of(:heavy_armor)}
+    it {should validate_presence_of(:simple_weapon)}
+    it {should validate_presence_of(:martial_weapon)}
+    it {should validate_presence_of(:ammunition)}
+    it {should validate_presence_of(:adventuring_supplies)}
+    it {should validate_presence_of(:assassins_blood)}
+    it {should validate_presence_of(:malice)}
+    it {should validate_presence_of(:midnight_tears)}
+    it {should validate_presence_of(:serpent_venom)}
+    it {should validate_presence_of(:truth_serum)}
+    it {should validate_presence_of(:oil_of_slipperiness)}
+    it {should validate_presence_of(:potion_of_climbing)}
+    it {should validate_presence_of(:potion_of_healing)}
+    it {should validate_presence_of(:potion_of_water_breathing)}
+    it {should validate_presence_of(:barge)}
+    it {should validate_presence_of(:coracle)}
+    it {should validate_presence_of(:double_hulled_sailing_canoe)}
+    it {should validate_presence_of(:keelboat)}
+    it {should validate_presence_of(:raft)}
+    it {should validate_presence_of(:single_hulled_sailing_canoe)}
+    it {should validate_presence_of(:ballista)}
+    it {should validate_presence_of(:cabin)}
+    it {should validate_presence_of(:magical_defenses)}
+    it {should validate_presence_of(:storage)}
+
+    it {should validate_numericality_of(:campaign_id)}
+    it {should validate_numericality_of(:week)}
+    it {should validate_numericality_of(:animal_products)}
+    it {should validate_numericality_of(:cloth)}
+    it {should validate_numericality_of(:farmed_goods)}
+    it {should validate_numericality_of(:food)}
+    it {should validate_numericality_of(:foraged_goods)}
+    it {should validate_numericality_of(:metal)}
+    it {should validate_numericality_of(:monster_parts)}
+    it {should validate_numericality_of(:stone)}
+    it {should validate_numericality_of(:wood)}
+    it {should validate_numericality_of(:light_armor)}
+    it {should validate_numericality_of(:medium_armor)}
+    it {should validate_numericality_of(:heavy_armor)}
+    it {should validate_numericality_of(:simple_weapon)}
+    it {should validate_numericality_of(:martial_weapon)}
+    it {should validate_numericality_of(:ammunition)}
+    it {should validate_numericality_of(:adventuring_supplies)}
+    it {should validate_numericality_of(:assassins_blood)}
+    it {should validate_numericality_of(:malice)}
+    it {should validate_numericality_of(:midnight_tears)}
+    it {should validate_numericality_of(:serpent_venom)}
+    it {should validate_numericality_of(:truth_serum)}
+    it {should validate_numericality_of(:oil_of_slipperiness)}
+    it {should validate_numericality_of(:potion_of_climbing)}
+    it {should validate_numericality_of(:potion_of_healing)}
+    it {should validate_numericality_of(:potion_of_water_breathing)}
+    it {should validate_numericality_of(:barge)}
+    it {should validate_numericality_of(:coracle)}
+    it {should validate_numericality_of(:double_hulled_sailing_canoe)}
+    it {should validate_numericality_of(:keelboat)}
+    it {should validate_numericality_of(:raft)}
+    it {should validate_numericality_of(:single_hulled_sailing_canoe)}
+    it {should validate_numericality_of(:ballista)}
+    it {should validate_numericality_of(:cabin)}
+    it {should validate_numericality_of(:magical_defenses)}
+    it {should validate_numericality_of(:storage)}
+  end
+
+  it 'exists and has attributes' do
+    form = create(:management_form)
+
+    expect(form).to be_a ManagementForm
+  end
 end
