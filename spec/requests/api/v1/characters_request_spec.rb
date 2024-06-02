db0953ec76cf96815d08b4a489bd599f3ac83762
Billy Wallace <wallace.billy1@gmail.com>
2024-06-02 12:00:07 -0600

    Add campaign characters index request spec tests

--- spec/requests/api/v1/characters_request_spec.rb
+++ spec/requests/api/v1/characters_request_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe "Characters API" do
   before(:each) do
     @character1 = create(:character)
+    @character2 = create(:character)
+    @character3 = create(:character)
+    @campaign1 = create(:campaign)
+    @campaign1.characters << [@character1, @character2, @character3]
   end
 
   describe "Character Show" do
@@ -82,6 +86,52 @@ RSpec.describe "Characters API" do
       expect(data[:errors]).to be_an(Array)
       expect(data[:errors].first[:status]).to eq("422")
       expect(data[:errors].first[:title]).to eq("Validation failed: Dnd race can't be blank")
+    end
+  end
+
+  describe "Campaign Characters Index" do
+    it "returns all characters and their attributes for a specific campaign" do
+      get "/api/v1/campaigns/#{@campaign1.id}/characters"
+
+      characters = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      expect(characters).to be_an(Array)
+      expect(characters.count).to eq(3)
+      
+      characters.each do |character|
+        expect(character).to have_key(:id)
+        expect(character[:id]).to be_a(String)
+  
+        expect(character[:attributes]).to have_key(:name)
+        expect(character[:attributes][:name]).to be_a(String)
+  
+        # expect(character[:attributes]).to have_key(:user_id)
+        # expect(character[:attributes][:user_id]).to be_an(String)
+  
+        expect(character[:attributes]).to have_key(:dnd_race)
+        expect(character[:attributes][:dnd_race]).to be_a(String)
+  
+        expect(character[:attributes]).to have_key(:dnd_class)
+        expect(character[:attributes][:dnd_class]).to be_a(String)
+
+        expect(character[:attributes]).to have_key(:image_url)
+      end
+    end
+
+    it "returns a 404 status and error message when an invalid campaign id is passed in" do 
+      get "/api/v1/campaigns/123123/characters" 
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(data[:errors]).to be_an(Array)
+      expect(data[:errors].first[:status]).to eq("404")
+      expect(data[:errors].first[:title]).to eq("Couldn't find Campaign with 'id'=123123")
     end
   end
 end
