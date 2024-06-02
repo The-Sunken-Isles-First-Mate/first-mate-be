89bc337de1fd63f83fc34addbc6a8543d1d2db7f
Billy Wallace <wallace.billy1@gmail.com>
2024-06-02 11:03:23 -0600

    Create route for character show

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :characters, only: [:show]
       resources :campaigns, only: [:show, :create, :update] do
         resources :items, only: [:index]
       end
