888ae524e6d7be4e6c1dc35241ec1c28053bf8d8
Billy Wallace <wallace.billy1@gmail.com>
2024-06-02 14:48:12 -0600

    Updated models and serializer to incorporate newly merged user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # has_many :user_campaigns
   # has_many :campaigns, through: :user_campaigns
-  # has_many :characters
+  has_many :characters
 
   validates :uid, :username, :token, presence: true
   validates :uid, uniqueness: true
