c36eb68ad402eb518eeb09dd04ddb2c0d23ea426
Billy Wallace <wallace.billy1@gmail.com>
2024-06-02 11:30:19 -0600

    Create character show controller

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::CharactersController < ApplicationController
     character = Character.find(params[:id])
     render json: CharacterSerializer.new(character)
   end
+
+  def create
+    character = Character.new(character_params)
+    character.save!
+    render json: CharacterSerializer.new(character), status: 201
+  end
+
+  private
+  def character_params
+    params.require(:character)
+          .permit(:name, 
+                  # :user_id, 
+                  :dnd_race, 
+                  :dnd_class, 
+                  :image_url)
+  end
 end
