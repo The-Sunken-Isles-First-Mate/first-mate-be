3f4039a52ee8fa849facc25af42bcacc5c115108
Billy Wallace <wallace.billy1@gmail.com>
2024-06-03 11:22:58 -0600

    Update routes for new namespacing and routing

--- config/routes.rb
+++ config/routes.rb
@@ -13,14 +13,14 @@ Rails.application.routes.draw do
       resources :characters, only: [:show, :create]
       resources :campaigns, only: [:show, :create, :update] do
         resources :items, only: [:index]
-        resources :characters, only: [:index]
       end
-      resources :items, only: [:show]
+      resources :items, only: [:show, :index]
       resources :management_forms, only: [:update]
       resources :campaign_items, only: [:create, :update]
       resources :users, only: [:show]
       resources :user_campaigns, only: [:create]
       get '/management_form', to: 'management_forms#show'
+      get '/campaigns/:id/characters', to: 'campaigns/characters#index'
     end
   end
 end
