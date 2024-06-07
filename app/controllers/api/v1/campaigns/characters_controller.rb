8acf491a8d248a4f0cb02bdb621ff292069d86c0
Jared Hobson <jaredmhobson@gmail.com>
2024-06-07 01:06:58 -0700

    Update CSV load for testing

--- app/controllers/api/v1/campaigns/characters_controller.rb
+++ app/controllers/api/v1/campaigns/characters_controller.rb
@@ -1,7 +1,10 @@
-class Api::V1::Campaigns::CharactersController < ApplicationController 
+class Api::V1::Campaigns::CharactersController < ApplicationController
   def index
     campaign = Campaign.find(params[:id])
     characters = campaign.characters
+    characters.map do |character|
+      character.character_image
+    end
     render json: CharacterSerializer.new(characters)
   end
 end
