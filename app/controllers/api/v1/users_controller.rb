840f4bdcc50848df73574533385ba8d43f808eb9
Billy Wallace <wallace.billy1@gmail.com>
2024-06-19 12:38:16 -0600

    Update user show action to utilize google oauth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,23 @@
 class Api::V1::UsersController < ApplicationController
   def show
-    if (username = params[:user][:username]) && (token = params[:user][:token])
-      user = User.find_or_create_by(uid: params[:id])
-      user.username = username.downcase
-      user.uid = params[:id]
-      user.token = token
-      user.save
-      render json: UserSerializer.new(user)
-    else
-      render json: { errors: [{status: '400', title:'Username or Token cannot be missing' }]},
-      status: :bad_request
+    user = User.find_or_create_by(uid: params[:id]) do |user|
+      user.username = params[:user][:username]
     end
+    user.save!
+    render json: UserSerializer.new(user)
   end
+
+  # def show
+  #   if (username = params[:user][:username]) && (token = params[:user][:token])
+  #     user = User.find_or_create_by(uid: params[:id])
+  #     user.username = username.downcase
+  #     user.uid = params[:id]
+  #     user.token = token
+  #     user.save
+  #     render json: UserSerializer.new(user)
+  #   else
+  #     render json: { errors: [{status: '400', title:'Username or Token cannot be missing' }]},
+  #     status: :bad_request
+  #   end
+  # end
 end
