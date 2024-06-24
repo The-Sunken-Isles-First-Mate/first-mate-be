927b7ee7cc277a1f8d18ef621da4b14d5ac38cd2
Grant Davis <grantdavis303@gmail.com>
2024-06-24 14:46:49 -0600

    Update Users Show for OAuth and Regular Authentication

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::UsersController < ApplicationController
   def show
-    user = User.find_or_create_by(uid: params[:id]) do |user|
-      user.username = params[:user][:username]
+    if params[:user][:password]
+      user = User.find_or_create_by(uid: params[:id]) do |user|
+        user.username = params[:user][:username]
+        user.password = params[:user][:password]
+      end
+      user.save!
+    else
+      user = User.find_or_create_by(uid: params[:id]) do |user|
+        user.username = params[:user][:username]
+        user.password = SecureRandom.base64.tr('+/=', 'Qrt')
+      end
+      user.save!
     end
-    user.save!
+
     render json: UserSerializer.new(user)
   end
 
