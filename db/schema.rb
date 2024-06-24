53f80d83f307b68d2a89e7d00926f6da62f8d7b6
Grant Davis <grantdavis303@gmail.com>
2024-06-24 14:45:55 -0600

    Create Password Digest Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_05_154955) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_24_175955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -156,6 +156,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_05_154955) do
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
